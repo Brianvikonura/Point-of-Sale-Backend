3543e459592c8d50cd709798bb120b9268c7c506
brianvikonura <vikonura@gmail.com>
2024-02-14 23:04:30 +0700

    Add register page and fix redirect URL

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', function () {
+    Route::get('home', function () {
         return view('pages.dashboard');
-    })->name('dashboard');
+    })->name('home');
 });
