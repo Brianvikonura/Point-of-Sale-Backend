513e34498df65d6b63d3fec7bb577be8296faae2
brianvikonura <vikonura@gmail.com>
2024-02-14 15:36:49 +0700

    Login and logout using Fortify

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.dashboard');
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard', function () {
+        return view('pages.dashboard');
+    })->name('dashboard');
 });
