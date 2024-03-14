b691fdf7ef1d416338de195b8416ffc33db55640
brianvikonura <vikonura@gmail.com>
2024-03-14 23:21:34 +0700

    feat: Create API to add discount

--- app/Http/Controllers/Api/DiscountController.php
+++ app/Http/Controllers/Api/DiscountController.php
@@ -17,4 +17,22 @@ class DiscountController extends Controller
             'data' => $discounts
         ], 200);
     }
+
+    // store
+    public function store(Request $request)
+    {
+        // validate the request
+        $request->validate([
+            'name' => 'required',
+            'value' => 'required',
+        ]);
+
+        // create the discount
+        $discount = \App\Models\Discount::create($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $discount
+        ], 200);
+    }
 }
